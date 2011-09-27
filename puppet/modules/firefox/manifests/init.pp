00dd98cff07cb85e13b14505e9e48ff4bd066af2
Jari Bakken <jari.bakken@gmail.com>
2011-09-27 13:50:58 +0200

    Various improvements

--- puppet/modules/firefox/manifests/init.pp
+++ puppet/modules/firefox/manifests/init.pp
@@ -5,11 +5,12 @@ class firefox {
     source => "file:///tmp/vagrant-puppet/modules-0/firefox/files/firefox-6.0.2.tar.bz2", # not ideal
     path => "/tmp/firefox.tar.bz2"
   }
-  
-  exec { "install-firefox": 
+
+  exec { "install-firefox":
     cwd => "/tmp",
     command => "tar jxvf firefox.tar.bz2",
     require => File['firefox-tarball'],
-    path => ["/usr/bin", "/bin"]
+    path => ["/usr/bin", "/bin"],
+    onlyif => "test `ls /tmp/firefox/firefox | wc -l` -eq 0"
   }
 }
