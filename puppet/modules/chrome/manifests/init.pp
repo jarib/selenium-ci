1ac3904cb06d07ff9335a78106391beaa433e687
Jari Bakken <jari.bakken@gmail.com>
2011-09-27 14:37:55 +0200

    Improve apt / chrome.

--- puppet/modules/chrome/manifests/init.pp
+++ puppet/modules/chrome/manifests/init.pp
@@ -1,11 +1,11 @@
 class chrome {
   $apt_source = "http://dl.google.com/linux/chrome/deb/"
   $google_deb_key = "http://dl-ssl.google.com/linux/linux_signing_key.pub" # should use https, but not supported by GET by default
-  
-  # this is probably not the right way to do this.
-  exec { "add-google-source":
-    command => "/bin/echo 'deb ${apt_source} stable main' >> /etc/apt/sources.list",
-    onlyif => "/usr/bin/test `grep -F ${apt_source} /etc/apt/sources.list | wc -l` -eq 0"
+
+  file { "google-apt-source":
+    ensure => file,
+    path => "/etc/apt/sources.list.d/google.list",
+    content => "deb ${apt_source} stable main"
   }
   
   exec { "add-google-deb-key":
@@ -14,11 +14,10 @@ class chrome {
     onlyif => "/usr/bin/test `apt-key list | grep -F Google | wc -l` -eq 0",
   }
   
-  Exec['add-google-source'] -> Exec['add-google-deb-key'] -> Exec['apt-update']
+  File['google-apt-source'] -> Exec['add-google-deb-key'] -> Exec['apt-update']
   
   package { "google-chrome-unstable": 
-    ensure => present,
-    require => [Exec['add-google-source'], Exec['add-google-deb-key']]
+    ensure => present
   }
   
   file { "chromedriver":
@@ -27,6 +26,5 @@ class chrome {
      source => "file:///tmp/vagrant-puppet/modules-0/chrome/files/chromedriver", # not ideal
      path => "/usr/bin/chromedriver"
    }
-
 }
 
