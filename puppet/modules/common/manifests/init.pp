85572b6b33daca0cd52a042933d7a481ad12a8e9
Jari Bakken <jari.bakken@gmail.com>
2011-10-01 23:37:12 +0200

    Only execute apt-get when necessary.

--- puppet/modules/common/manifests/init.pp
+++ puppet/modules/common/manifests/init.pp
@@ -4,7 +4,8 @@ class common {
   }
 
   exec { "apt-update":
-    command => "/usr/bin/apt-get update"
+    command => "/usr/bin/apt-get update",
+    onlyif  => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
   }
 
   Exec["apt-update"] -> Package <| |>
